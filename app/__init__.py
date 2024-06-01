0da2cd1f9ba377892e7c5436c4205b62fe2c248b
Kim Philot <andrephilot@gmail.com>
2024-06-01 10:07:48 -0600

    updating safety measures

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .routes import main
 def create_app():
     app = Flask(__name__)
 
-    # Retrieve the secret key from an environment variable set on Render
+    # Retrieve the secret key from an environment variable set on the hosting service
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
     app.register_blueprint(main)
