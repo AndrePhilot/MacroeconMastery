9c0a7c0f94ebf596d083eb9db42eebd473abc612
Kim Philot <andrephilot@gmail.com>
2024-10-28 15:54:57 -0600

    fixed a werkzeug vulnerability possible resource exhaustion

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, abort
 from .routes import main
 
 def create_app():
@@ -9,6 +9,13 @@ def create_app():
     # Retrieve the secret key from an environment variable set on the hosting service
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
+    app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+    @app.before_request
+    def enforce_request_limits():
+        if request.content_length and request.content_length > app.config['MAX_CONTENT_LENGTH']:
+            abort(413)  # Payload Too Large
+    
     app.register_blueprint(main)
 
     return app
