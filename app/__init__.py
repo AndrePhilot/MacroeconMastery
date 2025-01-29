aa5ee48065dc8bf6eb18f6e8a4bbdd4aba43d000
Andre Philot <andrephilot@gmail.com>
2025-01-29 09:50:12 -0600

    Update __init__.py
    Adding HSTS headers to enforce HTTPS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
-
 from flask import Flask, request, abort
+
 from .routes import main
 
 def create_app():
@@ -9,13 +9,20 @@ def create_app():
     # Retrieve the secret key from an environment variable set on the hosting service
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
+    # Set maximum content length to 10MB
     app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
     @app.before_request
     def enforce_request_limits():
         if request.content_length and request.content_length > app.config['MAX_CONTENT_LENGTH']:
             abort(413)  # Payload Too Large
-    
+
+    # Add HSTS headers to enforce HTTPS
+    @app.after_request
+    def add_security_headers(response):
+        response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+        return response
+
     app.register_blueprint(main)
 
     return app
